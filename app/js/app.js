e896a41d697b4bdc672fde2c908e6db066eaf2bd
Marish04 <94753536+Marish04@users.noreply.github.com>
2022-04-12 15:05:52 -0700

    add sections heder, hero

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,20 @@
-$(document).ready(function () {});
+const navLink = document.querySelector('.nav-link-explore');
+const navUl = document.querySelector('.explore-ul');
+const langLink = document.querySelector('.lang-link');
+const langUl = document.querySelector('.language-ul');
+const burger = document.querySelector('.burger');
+
+navLink.addEventListener('click', () => {
+    navUl.classList.toggle('open');
+    navLink.classList.toggle('open');
+})
+
+langLink.addEventListener('click', () => {
+    langUl.classList.toggle('open');
+    langLink.classList.toggle('open');
+})
+
+burger.addEventListener('click', () => {
+    burger.classList.toggle('open');
+})
+
